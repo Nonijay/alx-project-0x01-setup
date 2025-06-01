9f1b7c81c8fdf560e4ed5c957a1afd2528307f09
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-06-01 16:01:53 -0500

    update task

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,20 +1,48 @@
 import PostCard from "@/components/common/PostCard";
+import { PostProps } from "@/interfaces";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import React from "react";
 
-const Posts: React.FC = () => {
+const Posts: React.FC<PostProps[]> = ({ posts }) => {
+    console.log(posts)
     return (
-        <div className="h-screen">
+        <div className="flex flex-col h-screen">
             <Header/>
-            <div className="flex flex-col-2 justify-center items-center gap-3 px-5 py-2">
-                <h1 className="text-2x1 px-3 py-5">Posts Page</h1>
-                <PostCard/>
-            </div>
-            
-            <Footer />
+            <main className="p-5">
+                <div className="flex justify-between">
+                    <h1 className="text-2x1 font-semibold">Posts Content</h1>
+                    <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+                        Add Post
+                    </button>
+                </div>
+                <div className="grid grid-cols-3 gap-2">
+                    {
+                        posts?.map(({ title, body, userId, id }: PostProps, key:number ) =>
+                        (
+                            <PostCard 
+                                title={title} 
+                                body={body}
+                                userId={userId}
+                                id={id}
+                                key={key}
+                            />
+                        ))
+                    }
+                </div>   
+            </main>
         </div>
     )
 }
+export async function getStaticProps() {
+    const response = await
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    const posts = await response.json()
 
+    return {
+        props: {
+            posts
+        }
+    }
+}
 export default Posts
