5b706ddbe3751af478dc7cab2bec94c26ed9adc5
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-06-01 13:51:58 -0500

    Update Task

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,11 +1,15 @@
 import PostCard from "@/components/common/PostCard";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 import React from "react";
 
 const Posts: React.FC = () => {
     return (
         <div className="flex flex-col justify-center items-center h-screen">
+            <Header/>
             <h1 className="text-2x1 px-3 py-5">Posts Page</h1>
             <PostCard/>
+            <Footer />
         </div>
     )
 }
