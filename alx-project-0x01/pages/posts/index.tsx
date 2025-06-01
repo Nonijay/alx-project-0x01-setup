be89cea88a90e4929d6d3b6db72442ef19341811
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-06-01 17:42:40 -0500

    update task

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,50 +1,40 @@
 import PostCard from "@/components/common/PostCard";
-import { PostProps } from "@/interfaces";
 import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
-import React from "react";
-
-
+import { PostProps } from "@/interfaces";
 
 const Posts: React.FC<PostProps[]> = ({ posts }) => {
-    console.log(posts)
-    return (
-        <div className="flex flex-col h-screen">
-            <Header/>
-            <main className="p-5">
-                <div className="flex justify-between">
-                    <h1 className="text-2x1 font-semibold">Posts Content</h1>
-                    <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
-                        Add Post
-                    </button>
-                </div>
-                <div className="grid grid-cols-3 gap-2">
-                    {
-                        posts?.map(({ title, body, userId, id }: PostProps, key:number ) =>
-                        (
-                            <PostCard 
-                                title={title} 
-                                body={body}
-                                userId={userId}
-                                id={id}
-                                key={key}
-                            />
-                        ))
-                    }
-                </div>   
-            </main>
+    
+  console.log(posts)
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+        <h1 className=" text-2xl font-semibold">Post Content</h1>
+        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
         </div>
-    )
+        <div className="grid grid-cols-3 gap-2 ">
+          {
+            posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+              <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+            ))
+          }
+        </div>
+      </main>
+    </div>
+  )
 }
-export async function getStaticProps() {
-    const response = await
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    const posts = await response.json()
 
-    return {
-        props: {
-            posts
-        }
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
     }
+  }
 }
+
 export default Posts
