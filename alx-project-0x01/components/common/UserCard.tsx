93dde967862cff346871e151c33f7dd35872fbae
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-06-01 17:03:49 -0500

    update task

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -4,11 +4,10 @@ import { UserProps } from "@/interfaces";
 
 
 
-const Users: React.FC<UserProps[]> = ({ users }) => {
-    console.log(users);
+const Users: React.FC<UserProps> = ({ name, username, email, phone, website, address, company }) => {
     return(
         <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-x1 transition-shadow duration-300">
-
+            
         </div>
     )
 }
