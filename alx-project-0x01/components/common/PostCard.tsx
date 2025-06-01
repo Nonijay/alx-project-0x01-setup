315d58f05790d8037bf86701ef902ac53a9af7c6
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-06-01 15:34:19 -0500

    update task

--- alx-project-0x01/components/common/PostCard.tsx
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { PostProps } from "@/interfaces";
 
-const PostCard: React.FC = () => {
+const PostCard: React.FC<PostProps> = ({ title, body, userId, id }) => {
     return (
-        <h1>PostCard</h1>
-    )
-}
+        <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-x1 transition-shadow duration-300">
+            <div className="mb-4">
+                <h2 className="text-2x1 font-semibold text-gray-800">
+                    {title}
+                </h2>
+            </div>
+            <p className="text-gray-600">
+                {body}
+            </p>
+            <div className="mt-4 flex items-center justify-between text-sm text-gray-500">
+                <span>User ID: {userId}</span>
+                <span>Post ID: {id}</span>
+            </div>
+        </div>
+        
+    );
+};
 
 export default PostCard
--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,3 +1,10 @@
 export interface ButtonProps {
     title: string
 }
+
+export interface PostProps {
+    userId: number;
+    id: string;
+    title: string;
+    body: string;
+}
