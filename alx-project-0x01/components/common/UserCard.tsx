e6b113c692f4262a882308f092a92b9a802b955d
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-06-01 17:39:24 -0500

    update task

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -1,16 +1,18 @@
-import { UserProps } from "@/interfaces";
+import { UserProps } from "../../interfaces";
 
-
-
-
-
-const Users: React.FC<UserProps> = ({ name, username, email, phone, website, address, company }) => {
-    return(
-        <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-x1 transition-shadow duration-300">
-            
-        </div>
-    )
+const UserCard: React.FC<UserProps>=({name, email, phone, username, id, address, website, company})=>{
+return(
+    <div className="max-w-sm mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+      <div className="mb-4">
+        <h2 className="text-2xl font-semibold text-gray-800">{name}</h2>
+      </div>
+      <p className="text-gray-600">Email: {email}</p>
+      <p className="text-gray-600">Phone: {phone}</p>
+      <div className="mt-4 text-sm text-gray-500">
+        <p>Username: {username}</p>
+        <p>User ID: {id}</p>
+      </div>
+    </div>
+)
 }
-
-
-export default Users
+export default UserCard;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,16 +1,52 @@
 import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
-import React from "react";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
 
-const UserOne: React.FC = () => {
-    return (
-        <main className=" h-screen">
-            <Header />
-            <h1 className="text-2xl"> Welcome User</h1>
-            <Footer />
-        </main>
-        
+import { useState } from "react";
+import { UserData } from "@/interfaces";
+import UserModal from "@/components/common/UserModal";
+const Users: React.FC<UserProps[]> = ({posts}) => {  
+  const [isModalOpen, setModalOpen] = useState(false);
+    const [user, setUser] = useState<UserData | null>(null);
+  
+  
+    const handleAddUser = (newUser: UserData) => {
+      setUser({ ...newUser, id: posts.length + 1 });
+    };
+   
+  return(
+        <div>
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+        <h1 className=" text-2xl font-semibold">Users</h1>
+        <button onClick={()=> setModalOpen(true)} className="bg-blue-700 px-4 py-2 rounded-full text-white">
+          Add User</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {
+            posts.map(({ name, email,  username, id, address, phone, website, company}: UserProps, key: number) => (
+              <UserCard name={name} email={email} username={username} id={id} address={address} phone={phone} website={website} company={company} key={key} />
+            ))
+          }
+        </div>
+      </main>    
+      {isModalOpen && (
+        <UserModal onClose={() => setModalOpen(false)} onSubmit={handleAddUser} />
+      )}
+
+       </div>
     )
 }
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
 
-export default UserOne
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Users;
